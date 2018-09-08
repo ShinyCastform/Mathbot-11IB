43d29a3548a60366b856282b6c3ffdc64a3c87c6
pandaroux12 <35473641+pandaroux12@users.noreply.github.com>
2018-09-08 14:16:16 -0300

    Update CoreFile.py

--- CoreFile.py
+++ CoreFile.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 30 22:05:18 2018
+from Math_formulas import *
 
-@author: Bradyjomark
-"""
 def core():
     operation = input("Enter an operation : ")
     if operation == 'end':
         print("Program closed")
     
     elif operation == 'info':
-        print("Possible operations are : (I don't have any yet)")
-        return core()
+        print("Possible operations are : (area of a parallelogram)")
+        core()
+    elif operation == 'area of a parallelogram':
+        area_of_a_parallelogram()
+        core()
     else:
         print(str(operation) + " is sadly not supported by my program" + '\n' + 'type "info" to know all the supported programs')
-        return core()
+        core()
 
 core()
