586d0c9afc97770ebb576fda6c8f72bcc1b01d18
pandaroux12 <35473641+pandaroux12@users.noreply.github.com>
2018-09-09 11:51:10 -0300

    Update CoreFile.py

--- CoreFile.py
+++ CoreFile.py
@@ -6,10 +6,13 @@ def core():
         print("Program closed")
     
     elif operation == 'info':
-        print("Possible operations are : (area of a parallelogram)")
+        print("Possible operations are : (area of a parallelogram, area of a triangle)")
         core()
     elif operation == 'area of a parallelogram':
         area_of_a_parallelogram()
+        core()
+    elif operation == 'area of a triangle':
+        area_of_a_triangle()
         core()
     else:
         print(str(operation) + " is sadly not supported by my program" + '\n' + 'type "info" to know all the supported programs')
